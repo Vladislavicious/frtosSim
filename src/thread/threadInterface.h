09699b2a354ce79c3d869f1a4475e787e4595d73
Vladislavicious <vladshilov03@mail.ru>
2025-05-10 11:08:20 +0000

    started adding thread interaface

---
+++ src/thread/threadInterface.h
@@ -0,0 +1,12 @@
+#ifndef THREAD_INTERFACE_H_
+#define THREAD_INTERFACE_H_
+//---------------------------------------------------------------
+//       ThreadInterface interface definition:
+//---------------------------------------------------------------
+class ThreadInterface
+{
+public:
+  ThreadInterface();
+};
+//---------------------------------------------------------------
+#endif // THREAD_INTERFACE_H_
