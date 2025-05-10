46ca866f46af8fac7a902602f175b1e6cff36212
Vladislavicious <vladshilov03@mail.ru>
2025-05-10 12:35:57 +0000

    add taskInterface definition

---
+++ src/task/taskInterface.h
@@ -0,0 +1,12 @@
+#ifndef TASK_INTERFACE_H_
+#define TASK_INTERFACE_H_
+//---------------------------------------------------------------
+//       TaskInterface interface definition:
+//---------------------------------------------------------------
+class TaskInterface
+{
+public:
+  TaskInterface();
+};
+//---------------------------------------------------------------
+#endif // TASK_INTERFACE_H_
