88aabe72fd02f0dee637d26ac656c1bf7cee60b5
Vladislavicious <vladshilov03@mail.ru>
2025-05-13 16:08:30 +0000

    fixed hierarchy

--- src/task/buildTask.cpp
+++ src/task/buildTask.cpp
@@ -13,32 +13,11 @@ BuildTask::BuildTask()
 {
 }
 //---------------------------------------------------------------
-void BuildTask::SetInitialTime( TimeSV start )
+ErrorCode BuildTask::TaskOperation()
 {
-  startTime = start;
-}
-//---------------------------------------------------------------
-TimeSV BuildTask::GetInitialTime()
-{
-  return startTime;
-}
-//---------------------------------------------------------------
-bool BuildTask::HasEnded() const
-{
-  return ended;
-}
-//---------------------------------------------------------------
-TimeSV BuildTask::GetEndTime()
-{
-  return endTime;
-}
-//---------------------------------------------------------------
-void BuildTask::operator()()
-{
-  ended = false;
   for( int i = 0; i <= 10; ++i ) {
     std::this_thread::sleep_for( std::chrono::milliseconds( 50 ) ); // Имитация работы
   }
-  ended = true;
-  endTime = ApplicationGlobalInfo::Instance().GetCurrentAppTime();
-} 
+  return ErrorCode{ ErrorCodeEnum::ERR_OK };
+}
+//---------------------------------------------------------------
--- src/task/buildTask.h
+++ src/task/buildTask.h
@@ -2,9 +2,8 @@
 #define BUILD_TASK_H_
 
 #include "common.h"
-#include "time_sv.h"
+#include "taskInterface.h"
 #include "buildConfig.h"
-
 //---------------------------------------------------------------
 struct BuildProcess
 {
@@ -17,24 +16,12 @@ struct BuildProcess
 // BuildTask starts from main
 // BuildTask itself is not a std::thread functor
 
-class BuildTask
+class BuildTask : public TaskInterface
 {
 public:
-  // BuildTask( BuildConfig config );
   BuildTask();
-  void SetInitialTime( TimeSV start );
-  TimeSV GetInitialTime();
-  bool HasEnded() const;
-
-  TimeSV GetEndTime();
-
-  void operator()();
-
 private:
-  bool ended{ false };
-  TimeSV startTime{ 0 };
-  TimeSV endTime{ 0 };
-  ErrorCode result{ ErrorCodeEnum::ERR_TSK_NOT_STARTED };
+  ErrorCode TaskOperation() override;
 };
 //---------------------------------------------------------------
 #endif // BUILD_TASK_H_
