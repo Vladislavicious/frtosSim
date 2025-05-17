af72a44520c269232c78207f05e1f219a88655d9
Vladislavicious <vladshilov03@mail.ru>
2025-05-17 17:13:01 +0000

    made pessimistic systemConfig tests

--- test/src/systemConfig_tests.cpp
+++ test/src/systemConfig_tests.cpp
@@ -7,7 +7,6 @@ using json = nlohmann::json;
 
 using namespace std;
 
-
 static const std::string systemConfigDataPath = std::string( TEST_DATA_DIR ) + std::string( "/systemConfig/" );
 
 TEST( systemConfig, readGoodTypes )
@@ -24,4 +23,34 @@ TEST( systemConfig, readGoodTypes )
   expectedFirst.simulatorConfigPath = "asdjfkjasd";
 
   EXPECT_EQ( expectedFirst, config.GetMkConfigs()[0] );
+}
+
+TEST( systemConfig, readEmpty )
+{
+  SystemConfig config;
+  ConfigError result = config.CheckConfig( systemConfigDataPath + std::string( "SystemEmpty.json" ) );
+  EXPECT_TRUE( result.IsOk() );
+  EXPECT_STREQ( config.GetLoggerConfigPath().c_str(), "" );
+
+  std::vector<MkConfigStruct> expected;
+
+  EXPECT_EQ( expected, config.GetMkConfigs() );
+}
+
+TEST( systemConfig, readMkArray )
+{
+  SystemConfig config;
+  ConfigError result = config.CheckConfig( systemConfigDataPath + std::string( "SystemMany.json" ) );
+  EXPECT_TRUE( result.IsOk() );
+
+  EXPECT_STREQ( config.GetLoggerConfigPath().c_str(), "" );
+
+  MkConfigStruct expectedFirst;
+  expectedFirst.buildConfigPath = "1234";
+  expectedFirst.loggerConfigPath = "asdlfk";
+  expectedFirst.simulatorConfigPath = "asdjfkjasd";
+
+  std::vector<MkConfigStruct> expected{ expectedFirst, expectedFirst, expectedFirst };
+
+  EXPECT_EQ( expected, config.GetMkConfigs() );
 }
