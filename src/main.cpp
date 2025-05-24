b4b4995196c01d42a22fbfbc8c57f1171df6b401
Vladislavicious <vladshilov03@mail.ru>
2025-05-24 10:13:12 +0000

    add demonstrations for diplom

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,100 @@
 #include <iostream>
-#include "config.h"
-#include "time_sv.h"
+#include <string>
+#include <vector>
+#include <algorithm>
 
-static const char* const HEADER = "\nTest output\n\n";
+// Функция для вывода справки
+void printHelp() {
+  std::cout << "Application Help:\n";
+  std::cout << "Usage: app [options]\n";
+  std::cout << "Options:\n";
+  std::cout << "  -p <path_to_file>   Path to system configuration file\n";
+  std::cout << "  -h                  Display this help message\n";
+  std::cout << "  -s <speed>          Simulation speed\n";
+  std::cout << "  -t <template_path>  Create system template in specified folder\n";
+  std::cout << "\nDescription:\n";
+  std::cout << "If no configuration path is provided, the application checks for\n";
+  std::cout << "previously running systems and either starts the last active one\n";
+  std::cout << "or displays a message if none are found.\n";
+}
 
-int main( int argc, const char* argv[] )
-{
-  std::cout << HEADER;
+// Функция для проверки наличия ранее запущенных систем
+bool checkForRunningSystems() {
+  // Здесь должна быть реальная проверка, но для примера просто возвращаем false
+  return false;
+}
 
-  TimeSV vremya{ 0 };
-  std::cout << vremya.GetTimeStr() << "\n";
+// Функция для запуска последней активной системы
+void startLastActiveSystem() {
+  std::cout << "Starting last active system...\n";
+  // Реальная логика запуска
+}
+
+// Функция для создания шаблона конфигурации
+void createTemplate( const std::string& path ) {
+  std::cout << "Creating system template in: " << path << "\n";
+  // Реальная логика создания шаблона
+}
+
+int main( int argc, char* argv[] ) {
+  std::string configPath;
+  std::string speed;
+  std::string templatePath;
+  bool helpRequested = false;
+  bool templateRequested = false;
+
+  // Парсинг аргументов командной строки
+  for( int i = 1; i < argc; ++i ) {
+    std::string arg = argv[i];
+
+    if( arg == "-h" ) {
+      helpRequested = true;
+    }
+    else if( arg == "-p" && i + 1 < argc ) {
+      configPath = argv[++i];
+    }
+    else if( arg == "-s" && i + 1 < argc ) {
+      speed = argv[++i];
+    }
+    else if( arg == "-t" && i + 1 < argc ) {
+      templatePath = argv[++i];
+      templateRequested = true;
+    }
+  }
+
+  // Обработка запроса справки
+  if( helpRequested ) {
+    printHelp();
+    return 0;
+  }
+
+  // Обработка запроса создания шаблона
+  if( templateRequested ) {
+    createTemplate( templatePath );
+    return 0;
+  }
+
+  // Основная логика приложения
+  if( !configPath.empty() ) {
+    std::cout << "Starting system with configuration: " << configPath;
+    if( !speed.empty() ) {
+      std::cout << " at speed: " << speed;
+    }
+    std::cout << "\n";
+    // Здесь должна быть реальная логика запуска с указанным конфигом
+  }
+  else {
+    if( checkForRunningSystems() ) {
+      startLastActiveSystem();
+      if( !speed.empty() ) {
+        std::cout << "Setting simulation speed: " << speed << "\n";
+      }
+    }
+    else {
+      std::cout << "No active systems found and no configuration provided.\n";
+      std::cout << "Please specify a configuration file with -p or use -h for help.\n";
+    }
+  }
 
   return 0;
 }
--- test/src/logger_tests.cpp
+++ test/src/logger_tests.cpp
@@ -140,3 +140,112 @@ TEST( logger, ChangeLoggers ) {
   loggable.Log( LogLevel::ERROR, errorOutString );
 }
 //---------------------------------------------------------------
+FakeLogger GetLogger( std::string path )
+{
+  LoggerConfig appConfig;
+  ConfigError result = appConfig.ParseConfig( loggerDataPath + std::string( "fakeSim/" ) + path );
+  EXPECT_TRUE( result.IsOk() );
+  LoggerInterface* appLogInterface = LoggerFabric::GetLogger( appConfig );
+  EXPECT_TRUE( appLogInterface != nullptr );
+  FakeLogger appLoggable;
+  appLoggable.SetLogger( appLogInterface );
+
+  return appLoggable;
+}
+void del( long int ms )
+{
+  std::this_thread::sleep_for( std::chrono::milliseconds( ms ) );
+}
+
+struct HexCharStruct
+{
+  unsigned char c;
+  HexCharStruct( unsigned char _c ) : c( _c ) {}
+};
+
+inline std::ostream& operator<<( std::ostream& o, const HexCharStruct& hs )
+{
+  return ( o << " 0x" << std::hex << ( int )hs.c );
+}
+
+inline HexCharStruct hex( unsigned char _c )
+{
+  return HexCharStruct( _c );
+}
+//---------------------------------------------------------------
+TEST( logger, FakeAppStart ) {
+  FakeLogger appLoggable = GetLogger( "mainApp.json" );
+  FakeLogger sim1tx = GetLogger( "sim1.json" );
+  FakeLogger sim2tx = GetLogger( "sim2.json" );
+
+  appLoggable.Log( "start loading configs\n\n" );
+  del( 2 );
+  appLoggable.Log( LogLevel::INFO, "CONFIG - OK\n" );
+  appLoggable.Log( "start build tasks\n\n" );
+  del( 700 );
+  appLoggable.Log( LogLevel::INFO, "BUILD - OK\n\n" );
+  del( 10 );
+  appLoggable.Log( "start simulation \n\n" );
+
+  appLoggable.Log( LogLevel::INFO, "sim1 started\n" );
+  del( 1 );
+  sim1tx.Log( "peripheral initialization\n\n" );
+  sim1tx.Log( LogLevel::WARNING, "Bad clock init\n" );
+  appLoggable.Log( LogLevel::INFO, "sim2 started\n\n" );
+  del( 1 );
+  sim2tx.Log( "peripheral initialization\n\n" );
+
+
+  const int valStrLength = 14;
+  char value[valStrLength] = "hello, world!";
+  std::stringstream bytesValue;
+  for( int i = 0; i < valStrLength; i++ )
+  {
+    bytesValue << hex( value[i] );
+  }
+
+  std::string SendString = "send 10 bytes (uart)\n";
+  std::string ReceiveString = "received 10 bytes (uart):\n" + bytesValue.str() + "\n";
+
+  for( int i = 0; i < 10; i++ ) {
+    sim1tx.Log( LogLevel::INFO, SendString );
+    sim2tx.Log( LogLevel::INFO, ReceiveString );
+
+    del( 495 );
+
+    sim2tx.Log( LogLevel::INFO, SendString );
+    sim1tx.Log( LogLevel::INFO, ReceiveString );
+
+    appLoggable.Log( LogLevel::INFO, "alive: " + std::to_string( i ) + "\n" );
+    del( 495 );
+  }
+  // LoggerConfig consoleConfig;
+  // result = consoleConfig.ParseConfig( loggerDataPath + std::string( "consoleLog.json" ) );
+  // EXPECT_TRUE( result.IsOk() );
+
+
+  // FakeLogger loggable;
+  // loggable.SetLogger( appLogInterface );
+  // loggable.SetTimestamps( true );
+
+  // std::string noneOutString = "regular text\n";
+  // std::string outString = "info level text\n";
+
+  // std::string warningOutString = "text on warning level\n";
+  // std::string errorOutString = "text on error level\n";
+
+  // loggable.Log( LogLevel::NONE, noneOutString );
+  // loggable.Log( LogLevel::INFO, outString );
+  // loggable.Log( LogLevel::WARNING, warningOutString );
+  // loggable.Log( LogLevel::ERROR, errorOutString );
+
+  // LoggerInterface* consoleLogger = LoggerFabric::GetLogger( consoleConfig );
+  // EXPECT_TRUE( consoleLogger != nullptr );
+  // loggable.SetLogger( consoleLogger );
+
+  // loggable.Log( LogLevel::NONE, noneOutString );
+  // loggable.Log( LogLevel::INFO, outString );
+  // loggable.Log( LogLevel::WARNING, warningOutString );
+  // loggable.Log( LogLevel::ERROR, errorOutString );
+}
+//---------------------------------------------------------------
